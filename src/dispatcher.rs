e5680cbffde4ddf20d92d7d9b68c793b9266698f
Philipp Muens <raute1337@gmx.de>
2018-07-14 21:08:56 +0200

    Implement Dispatcher skeleton

---
+++ src/dispatcher.rs
@@ -0,0 +1,29 @@
+use regex::Regex;
+
+use config::{Config};
+
+pub struct Dispatcher<'a> {
+    config: &'a Config
+}
+
+impl<'a> Dispatcher<'a> {
+    pub fn new(config: &Config) -> Dispatcher {
+        let dispatcher = Dispatcher {
+            config: config
+        };
+        dispatcher
+    }
+
+    pub fn handle(&self, log_line: &str) {
+        for mapping in &self.config.mappings {
+            let re = Regex::new(&mapping.regex).unwrap();
+            if re.is_match(log_line) {
+                &self.emit(&mapping.event);
+            }
+        }
+    }
+
+    fn emit(&self, event: &str) {
+        println!("Emitting event \"{}\"", event);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,26 @@ extern crate serde_derive;
 extern crate serde;
 extern crate serde_json;
 extern crate chase;
+extern crate regex;
 
 mod config;
+mod dispatcher;
 
 use chase::Chaser;
+use dispatcher::Dispatcher;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config_file_path = &args[1];
 
     let config = config::parse(&config_file_path);
+    let dispatcher = Dispatcher::new(&config);
     let chaser = Chaser::new(&config.file_path);
 
     let (receiver, _) = chaser.run_channel().unwrap();
+
     loop {
         let log_line = &receiver.recv().unwrap().0;
-        println!("{}", log_line);
+        &dispatcher.handle(log_line);
     }
 }
