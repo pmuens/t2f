ac8a09087e08a6e35ee7b76034932f16c9935e7c
Philipp Muens <raute1337@gmx.de>
2018-07-14 20:48:26 +0200

    Implement config file parsing

---
+++ src/config.rs
@@ -0,0 +1,25 @@
+use std::io::prelude::*;
+use std::fs::File;
+
+use serde_json;
+
+#[derive(Deserialize, Debug)]
+pub struct Mapping {
+    pub regex: String,
+    pub event: String
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct Config {
+    pub file_path: String,
+    pub mappings: Vec<Mapping>
+}
+
+pub fn parse(ref file_path: &String) -> Config {
+    let mut file = File::open(file_path).expect("config file not found");
+    let mut content = String::new();
+    file.read_to_string(&mut content).expect("config file could not be parsed");
+
+    serde_json::from_str(&content).unwrap()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::env;
+
+#[macro_use]
+extern crate serde_derive;
+extern crate serde;
+extern crate serde_json;
+
+mod config;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let config_file_path = &args[1];
+
+    let config = config::parse(&config_file_path);
+
+    println!("{:?}", config.file_path);
+    println!("{:?}", config.mappings);
+    println!("{:?}", config.mappings[0].regex);
+    println!("{:?}", config.mappings[0].event);
+    println!("{:?}", config.mappings[1].regex);
+    println!("{:?}", config.mappings[1].event);
 }
